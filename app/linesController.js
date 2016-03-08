c6f8fe5b2e87b25a7aac1bf6727b7b7568d7531e
Pavel Yaroshchyk <pavel@pmbp.local>
2016-03-08 23:57:07 +1100

    basic ajax call added, need to scrap that, write test and start again

--- app/linesController.js
+++ app/linesController.js
@@ -1,2 +1,17 @@
 angular.module('spectralPlotter')
-	.controller('LinesController',['$scope', function($scope){}]);
+	.controller('LinesController',['$scope', '$http', function($scope, $http){
+		//--junk 
+		$scope.testData = {};
+		$scope.testData.testClick = function(item, event){
+			var testUrl = 'http://localhost:8080/atomicspectroscopy/api/data/lines/H/I';
+			var testResponse = $http.get(testUrl);	
+			testResponse.success(function(data, status, headers, config){
+				$scope.testData.result = data;
+				console.log(data);
+			});
+			testResponse.error(function(data, status, headers, config){
+				alert('failed call');
+			});
+		}
+		//-junk
+	}]);
