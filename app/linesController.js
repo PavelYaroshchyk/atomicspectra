b331fef790556a5537def034efd937ecae6179bd
Pavel Yaroshchyk <pavel@pmbp.local>
2016-03-09 23:38:49 +1100

    go button is disabled until element is selected

--- app/linesController.js
+++ app/linesController.js
@@ -2,9 +2,14 @@ angular.module('spectralPlotter')
 	.controller('LinesController',['$scope', '$http', function($scope, $http){
 	
 		$scope.linesData = {};
+		$scope.linesData.isAl = false;
+		$scope.linesData.isGo = false;
 		$scope.linesData.element = '';
 		$scope.linesData.selectAl = function(){
-			$scope.linesData.element = 'Al';
+			//$scope.linesData.element = 'Al';
+			$scope.linesData.isAl = !$scope.linesData.isAl;
+			$scope.linesData.element = $scope.linesData.isAl == true ? 'Al' : false;
+			$scope.linesData.isGo = $scope.linesData.isAl;
 		}
 
 		$scope.linesData.fetchLines = function(item, event){
