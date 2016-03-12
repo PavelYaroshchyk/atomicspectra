cd6a8f4cc48c79ec8227a5156ed75acff5ad9d12
Pavel Yaroshchyk <pavel.yaroshchyk@gmail.com>
2016-03-13 01:20:55 +1100

    added a mockup of a chart

--- app/linesController.js
+++ app/linesController.js
@@ -18,9 +18,46 @@ angular.module('spectralPlotter')
 			var linesResponse = $http.get(dataUrl);
 			linesResponse.success(function(data, status, headers, config){
 				$scope.linesData.result = data;
+
+
+				//from here
+
+
+
+				//charts
+				google.charts.load('current', {'packages':['corechart']});
+		      	google.charts.setOnLoadCallback(drawChart);
+
+		      	function drawChart() {
+		        	var data = google.visualization.arrayToDataTable([
+		          	['Wavelength', 'spectrum 1', 'spectrum 2'],
+		          	['label 1',  1,      2],
+		          	['label 2',  5,      1],
+		          	['label 3',  1,       10],
+		          	['label 4',  10,      1],
+		          	['label 5',  1,      1]
+		        	]);
+
+		        	var options = {
+		          	title: 'Company Performance',
+		          	curveType: 'function',
+		          	legend: { position: 'bottom' }
+		        	};
+
+		        	var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+		        	chart.draw(data, options);
+		      	}
+
+
+
+				//till here
+
+
 			});
 			linesResponse.error(function(data, status, headers, config){
 				console.log('failed get lines request' + status);
 			});
 		}
+
 	}]);
