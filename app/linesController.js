cf8ed5f9df79ef07a413941ec7e62212aa9a89bc
Pavel Yaroshchyk <pavel.yaroshchyk@gmail.com>
2016-03-23 23:04:29 +1100

    anchor schroll works

--- app/linesController.js
+++ app/linesController.js
@@ -1,74 +1,84 @@
 angular.module('spectralPlotter')
-	.controller('LinesController',['$scope', '$http', '$q', function($scope, $http, $q){
-		$scope.linesData = {};
-		$scope.linesData.isAl = false;
-		$scope.linesData.isCa = false;
-		$scope.linesData.isH = false;
-		$scope.linesData.isGo = false;
-		$scope.linesData.elements = [];
-		$scope.linesData.fwhm = 0.5;
-		$scope.linesData.numHorGr = 6;
+.controller('LinesController',['$scope', '$http', '$q', '$anchorScroll', '$location', function($scope, $http, $q, $anchorScroll, $location){
+	$scope.linesData = {};
+	$scope.linesData.isAl = false;
+	$scope.linesData.isCa = false;
+	$scope.linesData.isH = false;
+	$scope.linesData.isGo = false;
+	$scope.linesData.elements = [];
+	$scope.linesData.fwhm = 0.5;
+	$scope.linesData.numHorGr = 6;
 
-		var gatherElements = function(){
-			if ($scope.linesData.isAl && $scope.linesData.elements.indexOf('Al') == -1) $scope.linesData.elements.push('Al');
-			if ($scope.linesData.isH && $scope.linesData.elements.indexOf('H') == -1)   $scope.linesData.elements.push('H');
-			if ($scope.linesData.isCa && $scope.linesData.elements.indexOf('Ca') == -1) $scope.linesData.elements.push('Ca');
+	var gatherElements = function(){
+
+		$scope.linesData.elements.length = 0;
+		if ($scope.linesData.isAl && $scope.linesData.elements.indexOf('Al') == -1) $scope.linesData.elements.push('Al');
+		if ($scope.linesData.isH &&  $scope.linesData.elements.indexOf('H') == -1)  $scope.linesData.elements.push('H');
+		if ($scope.linesData.isCa && $scope.linesData.elements.indexOf('Ca') == -1) $scope.linesData.elements.push('Ca');
+	}
+
+	$scope.linesData.fetchLines = function(item, event){
+		
+		gatherElements();
+		var serviceUrl = 'http://localhost:8080/atomicspectroscopy/api/data/lines/';
+		var linesResponses = [];
+
+		for (var i = 0; i < $scope.linesData.elements.length; i++){
+			var dataUrl = serviceUrl + $scope.linesData.elements[i];	
+			linesResponses[i] = $http.get(dataUrl, {cache: true});
 		}
 
-		$scope.linesData.fetchLines = function(item, event){
-			
-			gatherElements();
-			var serviceUrl = 'http://localhost:8080/atomicspectroscopy/api/data/lines/';
-			var linesResponses = [];
+		$q.all(linesResponses).then(function(values) {
 
-			for (var i = 0; i < $scope.linesData.elements.length; i++){
-				var dataUrl = serviceUrl + $scope.linesData.elements[i];	
-				linesResponses[i] = $http.get(dataUrl);
-			}
-
-			$q.all(linesResponses).then(function(values) {
-
-				var data = [];
-				angular.forEach(values, function(value, key){
-					data = data.concat(value.data);
-				});
-				$scope.linesData.result = data;
-
-				var chart1 = {};
-			    chart1.type = "LineChart";
-			    chart1.cssStyle = "height:400px;";
-			    chart1.data = {"cols": [
-			        {id: "spec", label: "Spectrum", type: "number"},
-			        {id: "aSpec", label: $scope.linesData.elements.join(', '), type: "number"}], 
-			        "rows": getFormattedSpectrum(data, $scope.linesData.fwhm)
-				};
-
-			    chart1.options = {
-
-			    	explorer: {
-        				//maxZoomIn:0.1,
-        				keepInBounds: true,
-        				actions: ['dragToZoom', 'rightClickToReset'] 
-    				},
-
-    				legend: { position: 'bottom' },
-			  
-			        "displayExactValues": true,
-			        "vAxis": {
-			            "title": "Intensity/a.u."//, "gridlines": {"count": $scope.linesData.numHorGr}
-			        },
-			        "hAxis": {
-			            "title": "Wavelength/nm"
-			        }
-			    };
-
-			    chart1.formatters = {};
-			    $scope.linesData.linesChart = chart1;
-    			
+			var data = [];
+			angular.forEach(values, function(value, key){
+				data = data.concat(value.data);
 			});
-		}
-	}])
-	.directive('setClassWhenAtTop', ['$window', function($window) {
+			$scope.linesData.result = data;
+
+			var chart1 = {};
+		    chart1.type = "LineChart";
+		    chart1.cssStyle = "height:400px;";
+		    chart1.data = {"cols": [
+		        {id: "spec", label: "Spectrum", type: "number"},
+		        {id: "aSpec", label: $scope.linesData.elements.join(', '), type: "number"}], 
+		        "rows": getFormattedSpectrum(data, $scope.linesData.fwhm)
+			};
+
+		    chart1.options = {
+
+		    	explorer: {
+    				//maxZoomIn:0.1,
+    				keepInBounds: true,
+    				actions: ['dragToZoom', 'rightClickToReset'] 
+				},
+
+				legend: { position: 'bottom' },
+		  
+		        "displayExactValues": true,
+		        "vAxis": {
+		            "title": "Intensity/a.u."//, "gridlines": {"count": $scope.linesData.numHorGr}
+		        },
+		        "hAxis": {
+		            "title": "Wavelength/nm"
+		        }
+		    };
+
+		    chart1.formatters = {};
+		    $scope.linesData.linesChart = chart1;	
+		});
+	}
+
+	$scope.linesData.gotoAnchor = function(x) {
+	 	var oldHash = $location.hash();
+	 	$location.hash(x);
+	 	$anchorScroll.yOffset = 50;
+	 	$anchorScroll();
+	 	$location.hash(oldHash);
+	};
+}])
+
+.directive('setClassWhenAtTop', ['$window', function($window) {
     var $win = angular.element($window); // wrap window object as jQuery object
 
     return {
